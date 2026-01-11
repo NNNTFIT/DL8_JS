1b7071d7f4546f03e9f37bc004e0e90f08866f48
NNTFIT <nnt000fit@gmail.com>
2026-01-12 00:27:51 +0700

    dfgdfg

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ function openMenu(){
     container.classList.add("container-active")
     list.classList.add("list-active")
     nav.classList.add("nav-active")
+    document.body.classList.add("menu-open")
 }
 
 function closeMenu(){
     container.classList.remove("container-active")
     list.classList.remove("list-active")
     nav.classList.remove("nav-active")
+    document.body.classList.remove("menu-open")
 }
